a87a0f1ca680de06c169b49496a6e46b7512a0f1
tomkaX <tomacolyada@gmail.com>
2019-02-20 00:01:20 +0200

    2

--- R/test.r
+++ R/test.r
@@ -12,3 +12,8 @@ character_vector <- c("a", "b", "c")
 
 # Complete the code for boolean_vector
 boolean_vector <-c(TRUE, FALSE, TRUE)
+
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+# Assign days as names of poker_vector
+names(poker_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
