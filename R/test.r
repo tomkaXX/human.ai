b74f51abeb0e3a0db11d1f63def4aa232451f5ab
tomkaX <tomacolyada@gmail.com>
2019-02-20 22:23:44 +0200

    1

--- R/test.r
+++ R/test.r
@@ -25,3 +25,14 @@ poker_start <- poker_vector[c("Monday","Tuesday", "Wednesday")]
 # Calculate the average of the elements in poker_start
 mean(poker_start)
 
+# Box office Star Wars (in millions!)
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+
+# Construct matrix
+star_wars_matrix <- matrix(c(new_hope, empire_strikes, return_jedi), nrow = 3, byrow = TRUE)
+
+# Vectors region and titles, used for naming
+region <- c("US", "non-US")
+titles <- c("A New Hope", "The Empire Strikes Back", "Return of the Jedi")
