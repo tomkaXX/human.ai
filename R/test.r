5c64d9d76b7569d524d90c99b3e925e19b782a12
tomkaX <tomacolyada@gmail.com>
2019-02-23 20:39:38 +0200

    1

--- R/test.r
+++ R/test.r
@@ -49,6 +49,7 @@ star_wars_matrix
 
 # Construct star_wars_matrix
 box_office <- c(460.998, 314.4, 290.475, 247.900, 309.306, 165.8)
+
 star_wars_matrix <- matrix(box_office, nrow = 3, byrow = TRUE,
                            dimnames = list(c("A New Hope", "The Empire Strikes Back", "Return of the Jedi"), 
                                            c("US", "non-US")))
@@ -110,3 +111,4 @@ my_list <- list(my_vector, my_matrix, my_df)
 
 # Print the second element of the vector representing the actors
 shining_list[[2]][2]
+
