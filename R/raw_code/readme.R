223429b65ff95314c1738a24e5366708c5982d0b
tomkaX <tomacolyada@gmail.com>
2019-03-03 16:04:14 +0200

    update

--- R/raw_code/readme.R
+++ R/raw_code/readme.R
@@ -1,5 +1,10 @@
 # R packages
 
+Type the code you would need to install the `ggplot2` package.
+
+> install.packages("ggplot2")
+library(ggplot2)
+
 While you can use version to detect the version of R you're running, what code would you use to
 | check the version of R and the version of each package you're running?
 sessionInfo()
