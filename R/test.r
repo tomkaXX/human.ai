a130969af9c33c56a7bd30a613ac1ab7584fd402
tomkaX <tomacolyada@gmail.com>
2019-03-07 22:50:35 +0200

    1

--- R/test.r
+++ R/test.r
@@ -13,7 +13,6 @@ character_vector <- c("a", "b", "c")
 # Complete the code for boolean_vector
 boolean_vector <-c(TRUE, FALSE, TRUE)
 
-
 # Poker winnings from Monday to Friday
 poker_vector <- c(140, -50, 20, -120, 240)
 
